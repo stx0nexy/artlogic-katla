41ba214f3e4d89e53951eef7857ea1f520d782a7
Sofia Nesterenko <st.x0nexy@gmail.com>
2024-01-31 22:18:55 +0100

    Merge step14. Add methods for adding, updating and deleting HiveSections. Updated HiveSection model, created HiveSectionFormComponent with validation and control buttons.

--- KatlaSport.Services.Models/HiveManagement/HiveSectionListItem.cs
+++ KatlaSport.Services.Models/HiveManagement/HiveSectionListItem.cs
@@ -24,5 +24,10 @@
         /// Gets or sets a value indicating whether a hive is deleted.
         /// </summary>
         public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets a store hive ID.
+        /// </summary>
+        public int StoreHiveId { get; set; }
     }
 }
